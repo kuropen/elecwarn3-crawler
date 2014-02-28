bf1d370bd492a5b59122c3a2ec8b8a61976b304d
Hirochika Yuda <shinkai.sdpl@gmail.com>
2014-03-01 08:43:37 +0900

    コードフォーマットの統一

--- src/org/kuropen/elecwarnv3/ElecwarnBootstrap.java
+++ src/org/kuropen/elecwarnv3/ElecwarnBootstrap.java
@@ -1,15 +1,15 @@
 /*
  * Copyright (C) 2011-2013 Kuropen.
- * 
+ *
  * This file is part of the Electricity Warning Crawler, Version 3.
- * 
+ *
  * The Electricity Warning Crawler is free software:
  * you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * The Electricity Warning Crawler is distributed in the hope that
- * it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
@@ -32,61 +32,62 @@ import co.akabe.common.electricusage.ElectricUsageCSVParser;
  * 必要なパラメータは今回、完全にシェルスクリプト経由での起動を前提として環境変数に記述する方式をとった。
  */
 public class ElecwarnBootstrap {
-	public static void main (String[] args) {
-		// 環境変数の取得
-		String sendHost = System.getenv("WEBHOST");
-		String consumerKey = System.getenv("CONSUMER_KEY");
-		String consumerSecret = System.getenv("CONSUMER_SECRET");
-		String userKey = System.getenv("USER_KEY");
-		String userSecret = System.getenv("USER_SECRET");
-		if (sendHost == null || consumerKey == null || consumerSecret == null || userKey == null || userSecret == null) {
-			System.out.println("Missing environment valiable: Please set following environment valiable properly.");
-			System.out.println("Web host name as WEBHOST");
-			System.out.println("Twitter Consumer key as CONSUMER_KEY");
-			System.out.println("Twitter Consumer secret as CONSUMER_SECRET");
-			System.out.println("Twitter User token as USER_KEY");
-			System.out.println("Twitter User secret as USER_SECRET");
-			System.exit(1);
-		}
-		
-		//Twitterインスタンスの取得
-		TwitterUtilv3 twUtil = new TwitterUtilv3(consumerKey, consumerSecret, userKey, userSecret);
-		
-		//現在時刻の取得
-		Calendar cal = Calendar.getInstance();
-		int min = cal.get(Calendar.MINUTE);
-		
-		//Webサイト送信アクションの定義
-		GetInfoListener wsa = new WebSendAction(sendHost, twUtil);
-		
-		//情報取得アクションの定義
-		ArrayList<Action> actionList = new ArrayList<Action>();
-		
-		if(min % 5 == 0) {
-			actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Hokkaido, "hokkaido", wsa));
-			actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Tohoku, "tohoku", wsa));
-			actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Tokyo, "tokyo", wsa));
-			actionList.add(new GetInfoAction(ElectricUsageCSVParser.buildKyushuFormat(), "kyushu", wsa));
-		}
-		if(min % 6 == 0) {
-			actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Chubu, "chubu", wsa));
-			actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Kansai, "kansai", wsa));
-			actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Shikoku, "shikoku", wsa));
-		}
-		if(min % 10 == 0) {
-			actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Hokuriku, "hokuriku", wsa));
-			actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Chugoku, "chugoku", wsa));
-		}
-		if(min % 30 == 0) {
-			//30分に1回は定期ツイート（現在の使用率案内）
-			actionList.add(new PeriodicTweetAction(twUtil, cal));
-		}
-		
-		if(actionList.size() > 0) {
-			for(Action action : actionList) {
-				action.doAction();
-			}
-		}
-		
-	}
+    public static void main(String[] args) {
+        // 環境変数の取得
+        String sendHost = System.getenv("WEBHOST");
+        String consumerKey = System.getenv("CONSUMER_KEY");
+        String consumerSecret = System.getenv("CONSUMER_SECRET");
+        String userKey = System.getenv("USER_KEY");
+        String userSecret = System.getenv("USER_SECRET");
+        if (sendHost == null || consumerKey == null || consumerSecret == null || userKey == null || userSecret == null) {
+            System.out.println("Missing environment valiable: Please set following environment valiable properly.");
+            System.out.println("Web host name as WEBHOST");
+            System.out.println("Twitter Consumer key as CONSUMER_KEY");
+            System.out.println("Twitter Consumer secret as CONSUMER_SECRET");
+            System.out.println("Twitter User token as USER_KEY");
+            System.out.println("Twitter User secret as USER_SECRET");
+            System.exit(1);
+        }
+
+        //Twitterインスタンスの取得
+        TwitterUtilv3 twUtil = new TwitterUtilv3(consumerKey, consumerSecret, userKey, userSecret);
+
+        //現在時刻の取得
+        Calendar cal = Calendar.getInstance();
+        int min = cal.get(Calendar.MINUTE);
+
+        //Webサイト送信アクションの定義
+        GetInfoListener wsa = new WebSendAction(sendHost, twUtil);
+
+        //情報取得アクションの定義
+        ArrayList<Action> actionList = new ArrayList<Action>();
+
+        if (min % 5 == 0) {
+            actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Hokkaido, "hokkaido", wsa));
+            actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Tohoku, "tohoku", wsa));
+            actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Tokyo, "tokyo", wsa));
+            actionList.add(new GetInfoAction(ElectricUsageCSVParser.buildKyushuFormat(), "kyushu", wsa));
+        }
+        if (min % 6 == 0) {
+            actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Chubu, "chubu", wsa));
+            actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Kansai, "kansai", wsa));
+            actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Shikoku, "shikoku", wsa));
+        }
+        if (min % 10 == 0) {
+            actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Hokuriku, "hokuriku", wsa));
+            actionList.add(new GetInfoAction(ElectricUsageCSVParser.Format_Chugoku, "chugoku", wsa));
+        }
+        if (min % 30 == 0) {
+            //30分に1回は定期ツイート（現在の使用率案内）
+            actionList.add(new PeriodicTweetAction(twUtil, cal));
+        }
+
+        if (actionList.size() > 0) {
+            for (Action action : actionList) {
+                action.doAction();
+            }
+        }
+
+    }
+>>>>>>> d444723... コードフォーマットの統一、ElectricUsageの更新
 }
--- src/org/kuropen/elecwarnv3/util/TwitterUtilv3.java
+++ src/org/kuropen/elecwarnv3/util/TwitterUtilv3.java
@@ -26,34 +26,35 @@ import twitter4j.auth.AccessToken;
 
 public class TwitterUtilv3 {
 
-	private Twitter twitter;
-	
-	public TwitterUtilv3 (String cKey, String cSecret, String uKey, String uSecret) {
-		twitter = new TwitterFactory().getInstance();
-		twitter.setOAuthConsumer(cKey, cSecret);
-		AccessToken aToken = new AccessToken(uKey, uSecret);
-		twitter.setOAuthAccessToken(aToken);
-	}
-	
-	/**
-	 * ツイートする
-	 */
-	public void sendTweet(String content) {
-		sendTweet(content, false);
-	}
-	public void sendTweet(String content, boolean testflag) {
-		String status;
-		if(testflag) {
-			status = "【試験】" + content;
-		}else{
-			status = content;
-		}
-		try {
-			twitter.updateStatus(status);
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
+    private Twitter twitter;
+
+    public TwitterUtilv3(String cKey, String cSecret, String uKey, String uSecret) {
+        twitter = new TwitterFactory().getInstance();
+        twitter.setOAuthConsumer(cKey, cSecret);
+        AccessToken aToken = new AccessToken(uKey, uSecret);
+        twitter.setOAuthAccessToken(aToken);
+    }
+
+    /**
+     * ツイートする
+     */
+    public void sendTweet(String content) {
+        sendTweet(content, false);
+    }
+
+    public void sendTweet(String content, boolean testflag) {
+        String status;
+        if (testflag) {
+            status = "【試験】" + content;
+        } else {
+            status = content;
+        }
+        try {
+            twitter.updateStatus(status);
+        } catch (TwitterException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 }
